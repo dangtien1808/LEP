dbb2158477a9eb86f0459ea8726eb179dc3bc9d6
dangtien1808 <dangvantien1808@gmail.com>
2021-02-23 16:59:42 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\DropzoneController;
 |
 */
 
-Route::get('/', function(){
-   return view('welcome');
-});
-//Route::get('/', [DropzoneController::class, 'index']);
+//Route::get('/', function(){
+ //  return view('welcome');
+//});
+Route::get('/', [DropzoneController::class, 'index']);
 
 Route::get('upload', [DropzoneController::class, 'index']);
 Route::get('upload/fetch', [DropzoneController::class, 'fetch'])->name('upload.fetch');
